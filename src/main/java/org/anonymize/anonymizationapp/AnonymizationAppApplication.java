265ae2f50cbbe0319882e715f27fe29f6b99b0a7
Jake Murphy Smith <jakemurph996@gmail.com>
2017-11-27 12:45:11 +0000

    Commit

---
+++ src/main/java/org/anonymize/anonymizationapp/AnonymizationAppApplication.java
@@ -0,0 +1,12 @@
+package org.anonymize.anonymizationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymizationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymizationAppApplication.class, args);
+	}
+}
---
+++ src/test/java/org/anonymize/anonymizationapp/AnonymizationAppApplicationTests.java
@@ -0,0 +1,16 @@
+package org.anonymize.anonymizationapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnonymizationAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
