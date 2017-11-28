fcea3c4ab484d8990b55000a26e5d4f96f7e1061
Jake Murphy Smith <jakemurph996@gmail.com>
2017-11-28 11:32:08 +0000

    Adding commented out jpa dependencies

--- src/main/java/org/anonymize/anonymizationapp/controller/HelloController.java
+++ src/main/java/org/anonymize/anonymizationapp/controller/HelloController.java
@@ -2,7 +2,7 @@ package org.anonymize.anonymizationapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
