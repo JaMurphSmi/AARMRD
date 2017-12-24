c769fbf9b9f0ecc64aef115faae75210647585ab
Jake Murphy Smith <jakemurph996@gmail.com>
2017-12-24 19:58:59 +0000

    test for anonymization library

--- src/main/java/org/anonymize/anonymizationapp/controller/AnonymizationController.java
+++ src/main/java/org/anonymize/anonymizationapp/controller/AnonymizationController.java
@@ -1,5 +1,27 @@
 package org.anonymize.anonymizationapp.controller;
 
+
+// Importing ARX required modules, dependencies etc
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.deidentifier.arx.ARXAnonymizer;
+import org.deidentifier.arx.ARXConfiguration;
+import org.deidentifier.arx.ARXLattice.ARXNode;
+import org.deidentifier.arx.ARXResult;
+import org.deidentifier.arx.AttributeType.Hierarchy;
+import org.deidentifier.arx.AttributeType.Hierarchy.DefaultHierarchy;
+import org.deidentifier.arx.Data;
+import org.deidentifier.arx.Data.DefaultData;
+import org.deidentifier.arx.DataHandle;
+import org.deidentifier.arx.criteria.KAnonymity;
+import org.anonymize.anonymizationapp.model.AnonymizationBase;
+// ARX related stuff 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.PostMapping;
@@ -7,15 +29,75 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class AnonymizationController {
+public class AnonymizationController extends AnonymizationBase {
 
    @RequestMapping("/anonymize")
-   public String index(Model model) {
+   public String index(Model model) throws IOException {
 	   int anArray[] = {4,5,6,7,8,9,10};
 	   int secArray[];
 	   secArray = calcFigures(anArray);
 	   model.addAttribute("anonMessage", "This is where the anonymization will be placed");
 	   model.addAttribute("figures", secArray);
+	   //adding some hardcoded examples for ARX
+	// Define data
+       DefaultData data = Data.create();
+       data.add("age", "gender", "zipcode");
+       data.add("34", "male", "81667");
+       data.add("45", "female", "81675");
+       data.add("66", "male", "81925");
+       data.add("70", "female", "81931");
+       data.add("34", "female", "81931");
+       data.add("70", "male", "81931");
+       data.add("45", "male", "81931");
+
+       // Define hierarchies
+       DefaultHierarchy age = Hierarchy.create();
+       age.add("34", "<50", "*");
+       age.add("45", "<50", "*");
+       age.add("66", ">=50", "*");
+       age.add("70", ">=50", "*");
+
+       DefaultHierarchy gender = Hierarchy.create();
+       gender.add("male", "*");
+       gender.add("female", "*");
+
+       // Only excerpts for readability
+       DefaultHierarchy zipcode = Hierarchy.create();
+       zipcode.add("81667", "8166*", "816**", "81***", "8****", "*****");
+       zipcode.add("81675", "8167*", "816**", "81***", "8****", "*****");
+       zipcode.add("81925", "8192*", "819**", "81***", "8****", "*****");
+       zipcode.add("81931", "8193*", "819**", "81***", "8****", "*****");
+
+       data.getDefinition().setAttributeType("age", age);
+       data.getDefinition().setAttributeType("gender", gender);
+       data.getDefinition().setAttributeType("zipcode", zipcode);
+
+       // Create an instance of the anonymizer
+       ARXAnonymizer anonymizer = new ARXAnonymizer();
+       ARXConfiguration config = ARXConfiguration.create();
+       config.addPrivacyModel(new KAnonymity(3));
+       config.setMaxOutliers(0d);
+
+       ARXResult result = anonymizer.anonymize(data, config);
+
+       // Print info
+       printResult(result, data);
+
+       // Process results
+       System.out.println(" - Transformed data:");
+       Iterator<String[]> transformed = result.getOutput(false).iterator();
+       while (transformed.hasNext()) {
+           System.out.print("   ");
+           System.out.println(Arrays.toString(transformed.next()));
+  }
+
+	   
+	   
+	   
+	   
+	   
+	   
+	   
       return "anonymize";
    }
    
