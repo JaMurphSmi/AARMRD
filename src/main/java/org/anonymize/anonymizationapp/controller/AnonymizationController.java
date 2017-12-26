df53db85f4b8df004043b0ec6d6d19bd2bd23594
Jake Murphy Smith <jakemurph996@gmail.com>
2017-12-26 12:23:06 +0000

    adding more to test

--- src/main/java/org/anonymize/anonymizationapp/controller/AnonymizationController.java
+++ src/main/java/org/anonymize/anonymizationapp/controller/AnonymizationController.java
@@ -36,17 +36,19 @@ public class AnonymizationController extends AnonymizationBase {
 	   //adding some hardcoded examples for ARX
 	// Define data
        DefaultData data = Data.create();
-       data.add("age", "gender", "zipcode");
-       data.add("34", "male", "81667");
-       data.add("45", "female", "81675");
-       data.add("66", "male", "81925");
-       data.add("70", "female", "81931");
-       data.add("34", "female", "81931");
-       data.add("70", "male", "81931");
-       data.add("45", "male", "81931");
+       data.add("age", "gender", "zipcode", "phoneno");
+       data.add("34", "male", "81667", "083127869");
+       data.add("45", "female", "81675", "083125963");
+       data.add("66", "male", "81925", "084437546");
+       data.add("70", "female", "81931", "084431278");
+       data.add("34", "female", "81931", "085155078");
+       data.add("70", "male", "81931", "085155412");
+       data.add("45", "male", "81931", "085153436");
+       data.add("32", "male", "81931", "083127569");
 
-       // Define hierarchies
+       // Manually define hierarchies
        DefaultHierarchy age = Hierarchy.create();
+       age.add("32", "<50", "*");
        age.add("34", "<50", "*");
        age.add("45", "<50", "*");
        age.add("66", ">=50", "*");
@@ -62,10 +64,23 @@ public class AnonymizationController extends AnonymizationBase {
        zipcode.add("81675", "8167*", "816**", "81***", "8****", "*****");
        zipcode.add("81925", "8192*", "819**", "81***", "8****", "*****");
        zipcode.add("81931", "8193*", "819**", "81***", "8****", "*****");
-
+       
+       DefaultHierarchy phoneno = Hierarchy.create();
+       phoneno.add("083127869", "0831278**", "08312****", "083******");
+       phoneno.add("083125963", "0831259**", "08312****", "083******"); 
+       phoneno.add("083127569", "0831275**", "08312****", "083******");
+       phoneno.add("084437546", "0844375**", "08443****", "084******");
+       phoneno.add("084431278", "0844312**", "08443****", "084******");
+       phoneno.add("085155078", "0851550**", "08515****", "085******"); 
+       phoneno.add("085155412", "0851554**", "08515****", "085******");
+       phoneno.add("085153436", "0851534**", "08515****", "085******");
+       
+       
+       
        data.getDefinition().setAttributeType("age", age);
        data.getDefinition().setAttributeType("gender", gender);
        data.getDefinition().setAttributeType("zipcode", zipcode);
+       data.getDefinition().setAttributeType("phoneno", phoneno);
 
        // Create an instance of the anonymizer
        ARXAnonymizer anonymizer = new ARXAnonymizer();
