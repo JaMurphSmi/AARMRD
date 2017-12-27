f2ca0860844b547d9a060e40060c4107b341a4c9
Jake Murphy Smith <jakemurph996@gmail.com>
2017-12-27 00:53:32 +0000

    Making application read from files

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ To implement the anonymization functionality it is intended to make use of the o
 ARX (C) 2012-2017 Fabian Prasser, Florian Kohlmayer and Contributors.
 
 In the future it is also hoped to incorporate other anonymization suites to extend functionality and diversify options.
+
+****File Format Notes:
+
+For best results
+
+Notes for input data file formats: 
+CSV: Please ensure the data contained within is separated by a semi-colon, ';' character and 	 	 	 distinguish lines via a carriage return
+	 Place the headings for your fields on the top of the file ie age;gender;postcode;phoneno 
+	 Then place all corresponding data rows beneath ie 35;male;84324;086143565
+	 
+Notes for hierarchy file formats:
+CSV: No heading is required for this data file, all that is required is your defined hierarchy, 	 	 separated by a ';' ie age hierarchy: 34;<50;<=65;*
+										  43;<50;<=65;*
+										  24;<30;<50;<=65;*
--- src/main/java/org/anonymize/anonymizationapp/controller/AnonymizationController.java
+++ src/main/java/org/anonymize/anonymizationapp/controller/AnonymizationController.java
@@ -3,6 +3,7 @@ package org.anonymize.anonymizationapp.controller;
 
 // Importing ARX required modules, dependencies etc
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -33,73 +34,46 @@ public class AnonymizationController extends AnonymizationBase {
 	   secArray = calcFigures(anArray);
 	   model.addAttribute("anonMessage", "This is where the anonymization will be placed");
 	   model.addAttribute("figures", secArray);
+	   
 	   //adding some hardcoded examples for ARX
-	// Define data
-       DefaultData data = Data.create();
-       data.add("age", "gender", "zipcode", "phoneno");
-       data.add("34", "male", "81667", "083127869");
-       data.add("45", "female", "81675", "083125963");
-       data.add("66", "male", "81925", "084437546");
-       data.add("70", "female", "81931", "084431278");
-       data.add("34", "female", "81931", "085155078");
-       data.add("70", "male", "81931", "085155412");
-       data.add("45", "male", "81931", "085153436");
-       data.add("32", "male", "81931", "083127569");
-
-       // Manually define hierarchies
-       DefaultHierarchy age = Hierarchy.create();
-       age.add("32", "<50", "*");
-       age.add("34", "<50", "*");
-       age.add("45", "<50", "*");
-       age.add("66", ">=50", "*");
-       age.add("70", ">=50", "*");
-
-       DefaultHierarchy gender = Hierarchy.create();
-       gender.add("male", "*");
-       gender.add("female", "*");
-
-       // Need to add a hierarchy for each individual field
-       DefaultHierarchy zipcode = Hierarchy.create();
-       zipcode.add("81667", "8166*", "816**", "81***", "8****", "*****");
-       zipcode.add("81675", "8167*", "816**", "81***", "8****", "*****");
-       zipcode.add("81925", "8192*", "819**", "81***", "8****", "*****");
-       zipcode.add("81931", "8193*", "819**", "81***", "8****", "*****");
+	   ///////removed for input from file
+       //adding some harcoded examples for ARX
+	   
+       //implementing a hardcoded, local, imported file here
+       //will never be multiple 'data', or 'anonymizer', or 'config', or 'result' instance
+       Data data = Data.create("src/main/resources/templates/data/test_data.csv", StandardCharsets.UTF_8, ';');
        
-       DefaultHierarchy phoneno = Hierarchy.create();
-       phoneno.add("083127869", "0831278**", "08312****", "083******");
-       phoneno.add("083125963", "0831259**", "08312****", "083******"); 
-       phoneno.add("083127569", "0831275**", "08312****", "083******");
-       phoneno.add("084437546", "0844375**", "08443****", "084******");
-       phoneno.add("084431278", "0844312**", "08443****", "084******");
-       phoneno.add("085155078", "0851550**", "08515****", "085******"); 
-       phoneno.add("085155412", "0851554**", "08515****", "085******");
-       phoneno.add("085153436", "0851534**", "08515****", "085******");
-       
-       
-       
-       data.getDefinition().setAttributeType("age", age);
-       data.getDefinition().setAttributeType("gender", gender);
-       data.getDefinition().setAttributeType("zipcode", zipcode);
-       data.getDefinition().setAttributeType("phoneno", phoneno);
+       // Define input files
+       data.getDefinition().setAttributeType("age", Hierarchy.create("src/main/resources/templates/hierarchy/test_age.csv", StandardCharsets.UTF_8, ';'));
+       data.getDefinition().setAttributeType("gender", Hierarchy.create("src/main/resources/templates/hierarchy/test_gender.csv", StandardCharsets.UTF_8, ';'));
+       data.getDefinition().setAttributeType("zipcode", Hierarchy.create("src/main/resources/templates/hierarchy/test_zipcode.csv", StandardCharsets.UTF_8, ';'));
+       data.getDefinition().setAttributeType("phoneno", Hierarchy.create("src/main/resources/templates/hierarchy/test_phoneno.csv", StandardCharsets.UTF_8, ';'));
 
        // Create an instance of the anonymizer
        ARXAnonymizer anonymizer = new ARXAnonymizer();
+       
+       // Execute the algorithm
        ARXConfiguration config = ARXConfiguration.create();
-       config.addPrivacyModel(new KAnonymity(3));
+       config.addPrivacyModel(new KAnonymity(2));
        config.setMaxOutliers(0d);
-
        ARXResult result = anonymizer.anonymize(data, config);
-
+       
        // Print info
        printResult(result, data);
-
-       // Process results
+       
+       // Write results
+       System.out.print(" - Writing data...");
+       result.getOutput(false).save("src/main/resources/templates/output/test_anonymized.csv", ';');
+       System.out.println("Done!");
+       
+    // Process results
        System.out.println(" - Transformed data:");
        Iterator<String[]> transformed = result.getOutput(false).iterator();
        while (transformed.hasNext()) {
            System.out.print("   ");
            System.out.println(Arrays.toString(transformed.next()));
-  }   
+       }  
+       
       return "anonymize";
    }
    
