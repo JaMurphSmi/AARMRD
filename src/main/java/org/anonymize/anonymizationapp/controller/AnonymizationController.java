a5711efbb5c7d966162b9514fe98e9d53f739c71
Jake Murphy Smith <jakemurph996@gmail.com>
2018-01-06 14:04:28 +0000

    benefit configuration

--- src/main/java/org/anonymize/anonymizationapp/controller/AnonymizationController.java
+++ src/main/java/org/anonymize/anonymizationapp/controller/AnonymizationController.java
@@ -66,6 +66,7 @@ import org.deidentifier.arx.criteria.HierarchicalDistanceTCloseness;
 import org.deidentifier.arx.criteria.Inclusion;
 import org.deidentifier.arx.criteria.KAnonymity;
 import org.deidentifier.arx.criteria.KMap;
+import org.deidentifier.arx.criteria.ProfitabilityJournalistNoAttack;
 import org.deidentifier.arx.criteria.ProfitabilityProsecutorNoAttack;
 import org.deidentifier.arx.criteria.EntropyLDiversity;
 import org.deidentifier.arx.criteria.RecursiveCLDiversity;
@@ -895,10 +896,6 @@ public class AnonymizationController extends AnonymizationBase {
        System.out.println("\n - Quasi-identifiers with values (in percent) for 46:");
        analyzeAttributes(data46.getHandle());
        
-       System.out.print(" - Writing data...");
-       //result45.getOutput(false).save("src/main/resources/templates/output/test_anonymized46.csv", ';');
-       System.out.println("Done!");
-       
        ///////////////////////////////////////////////////
        // EXAMPLE 48 IS USEFUL
        ///////////////////////////////////////////////////
@@ -908,30 +905,80 @@ public class AnonymizationController extends AnonymizationBase {
        //////////////////////////////////////////
        // EXAMPLE 49 HERE
        //////////////////////////////////////////
+       // EXAMPLE 50 HERE
+       //////////////////////////////////////////
        
-       Data data49 = createData("adult");
+       Data data49 = createData("adult");// used for EXAMPLE 49, 50, 51 
+       
+       // added through example 50, removed for EXAMPLE 51 to improve computation time
+       /*DataSubset subset = DataSubset.create(data49, DataSelector.create(data49).field("sex").equals("Male"));
+
+    // Config from PLOS|ONE paper
+       solve(data49, ARXCostBenefitConfiguration.create()
+                                              .setAdversaryCost(4d)
+                                              .setAdversaryGain(300d)
+                                              .setPublisherLoss(300d)
+                                              .setPublisherBenefit(1200d), subset);
+
+       // Lower costs for the attacker
+       solve(data49, ARXCostBenefitConfiguration.create()
+                                              .setAdversaryCost(2d)
+                                              .setAdversaryGain(300d)
+                                              .setPublisherLoss(300d)
+                                              .setPublisherBenefit(1200d), subset);
+
+       // Lower costs and more gain for the attacker
+       solve(data49, ARXCostBenefitConfiguration.create()
+                                              .setAdversaryCost(2d)
+                                              .setAdversaryGain(600d)
+                                              .setPublisherLoss(300d)
+                                              .setPublisherBenefit(1200d), subset);
+
+       // Even more gain for the attacker
+       solve(data49, ARXCostBenefitConfiguration.create()
+                                              .setAdversaryCost(2d)
+                                              .setAdversaryGain(1200d)
+                                              .setPublisherLoss(300d)
+                                              .setPublisherBenefit(1200d), subset);*/
        
        // Config from PLOS|ONE paper
-       solve(data49, ARXCostBenefitConfiguration.create()
+       solve(data, ARXCostBenefitConfiguration.create()
                                               .setAdversaryCost(4d)
                                               .setAdversaryGain(300d)
                                               .setPublisherLoss(300d)
                                               .setPublisherBenefit(1200d));
 
-       // Fewer costs
-       solve(data49, ARXCostBenefitConfiguration.create()
-                                              .setAdversaryCost(2d)
-                                              .setAdversaryGain(600d)
-                                              .setPublisherLoss(300d)
+       // Larger publisher loss
+       solve(data, ARXCostBenefitConfiguration.create()
+                                              .setAdversaryCost(4d)
+                                              .setAdversaryGain(300d)
+                                              .setPublisherLoss(600d)
                                               .setPublisherBenefit(1200d));
 
-       // More costs, more gain
-       solve(data49, ARXCostBenefitConfiguration.create()
-                                              .setAdversaryCost(1d)
-                                              .setAdversaryGain(1200d)
-                                              .setPublisherLoss(300d)
+       // Even larger publisher loss
+       solve(data, ARXCostBenefitConfiguration.create()
+                                              .setAdversaryCost(4d)
+                                              .setAdversaryGain(300d)
+                                              .setPublisherLoss(1200d)
+                                              .setPublisherBenefit(1200d));
+
+       // Larger publisher loss and less adversary costs
+       solve(data, ARXCostBenefitConfiguration.create()
+                                              .setAdversaryCost(2d)
+                                              .setAdversaryGain(300d)
+                                              .setPublisherLoss(600d)
                                               .setPublisherBenefit(1200d));
        
+       // Config from PLOS|ONE paper but publisher loss and benefit changed with each other
+       solve(data, ARXCostBenefitConfiguration.create()
+                                              .setAdversaryCost(4d)
+                                              .setAdversaryGain(300d)
+                                              .setPublisherLoss(1200d)
+                                              .setPublisherBenefit(300d));
+
+       System.out.print(" - Writing data...");
+       //result45.getOutput(false).save("src/main/resources/templates/output/test_anonymized46.csv", ';');
+       System.out.println("Done!");
        
       return "anonymize";
    }
@@ -985,6 +1032,47 @@ public class AnonymizationController extends AnonymizationBase {
        
        // Print stuff
        System.out.println("Data: " + data.getHandle().getView().getNumRows() + " records with " + data.getDefinition().getQuasiIdentifyingAttributes().size() + " quasi-identifiers");
+       System.out.println(" - Configuration: " + config.toString());
+       System.out.println(" - Policies available: " + result.getLattice().getSize());
+       System.out.println(" - Solution: " + Arrays.toString(node.getTransformation()));
+       System.out.println("   * Optimal: " + result.getLattice().isComplete());
+       System.out.println("   * Time needed: " + result.getTime() + "[ms]");
+       for (QualityMetadata<?> metadata : node.getLowestScore().getMetadata()) {
+           System.out.println("   * " + metadata.getParameter() + ": " + metadata.getValue());
+       }
+       System.out.println("   * Suppressed records: " + handle.getStatistics().getEquivalenceClassStatistics().getNumberOfOutlyingTuples());
+
+   }
+   
+   //////////////// example 50
+   private static void solve(Data data, ARXCostBenefitConfiguration config, DataSubset subset) throws IOException {
+       
+       // Release
+       data.getHandle().release();
+       
+       // Configure
+       ARXConfiguration arxconfig = ARXConfiguration.create();
+       arxconfig.setCostBenefitConfiguration(config);
+       
+       // Create model for measuring publisher's benefit
+       MetricSDNMPublisherPayout maximizePublisherPayout = Metric.createPublisherPayoutMetric(true);
+       
+       // Create privacy model for the game-theoretic approach
+       ProfitabilityJournalistNoAttack profitability = new ProfitabilityJournalistNoAttack(subset);
+       
+       // Configure ARX
+       arxconfig.setMaxOutliers(1d);
+       arxconfig.setQualityModel(maximizePublisherPayout);
+       arxconfig.addPrivacyModel(profitability);
+
+       // Anonymize
+       ARXAnonymizer anonymizer = new ARXAnonymizer();
+       ARXResult result = anonymizer.anonymize(data, arxconfig);
+       ARXNode node = result.getGlobalOptimum();
+       DataHandle handle = result.getOutput(node, false).getView();
+       
+       // Print stuff
+       System.out.println("Data example 50: " + data.getHandle().getView().getNumRows() + " records with " + data.getDefinition().getQuasiIdentifyingAttributes().size() + " quasi-identifiers");
        System.out.println(" - Configuration: " + config.toString());
        System.out.println(" - Policies available: " + result.getLattice().getSize());
        System.out.println(" - Solution: " + Arrays.toString(node.getTransformation()));
