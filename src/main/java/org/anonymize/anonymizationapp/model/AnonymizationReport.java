27213f4b690ff7851bd3a896b833e7253d078e8d
Jake Murphy Smith <jakemurph996@gmail.com>
2018-03-29 18:12:37 +0100

    attempting changes for a report using jasper reports, very basic, no real progress

---
+++ src/main/java/org/anonymize/anonymizationapp/model/AnonymizationReport.java
@@ -0,0 +1,14 @@
+package org.anonymize.anonymizationapp.model;
+
+public class AnonymizationReport {
+	private String country;
+	private String threshold;
+	private String prosecutorRecordsAtRisk;
+	private String prosecutorHighestRisk;
+	private String prosecutorSuccessRate;
+	private String journalistRecordsAtRisk;
+	private String journalistHighestRisk;
+	private String journalistSuccessRate;
+	private String marketerStat;
+
+}
